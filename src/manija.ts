7c62261777e025acbff3769085ad4d2be1522587
VickyLew17 <victoria.lew17@gmail.com>
2026-06-09 15:55:12 -0300

    MANIJA

--- src/manija.ts
+++ src/manija.ts
@@ -1,5 +1,4 @@
 export function generarLorem(n: number): string {
-  // COMPLETAR: Implementar la función generarLorem que reciba un número n y devuelva una cadena de texto con la frase "Lorem Ipsum" repetida n veces, separada por espacios. Por ejemplo, si n es 3, la función debería devolver "Lorem Ipsum Lorem Ipsum Lorem Ipsum".
 
   if (n === 1){
     return "Lorem Ipsum."
@@ -10,4 +9,4 @@ export function generarLorem(n: number): string {
 
 }
 
-console.log(generarLorem(20));
+console.log(generarLorem(5));
